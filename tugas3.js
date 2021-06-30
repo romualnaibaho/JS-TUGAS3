04e9bde7a2e1fc00259d169a76d5677df8323039
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:36:16 +0700

    js

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+ function ubah_nilai(nilai){
+     nilai = "updated value";
+
+     return nilai;
+ }
+
+ var before = "old value";
+ 
+ console.log("Nilai Lama :", before);
+
+ console.log("Nilai Baru :", ubah_nilai(before));
